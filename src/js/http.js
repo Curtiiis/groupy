fd2c01afb53a1ae8dca506b731c18b36540726d6
Curtis <curtis@live.fr>
2022-05-16 19:11:42 +0200

    Frontend groupy

--- src/js/http.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: "https://groupy-app.herokuapp.com/api/",
-  headers: {
-    Authorization: "Bearer " + localStorage.getItem("token")
-  }
-});
